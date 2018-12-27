d6791542466cf17a8a14800f4a0d6836fa859196
First Last <youremailaddress@example.com>
2018-12-27 14:07:40 -0500

    display value functionsa added but clear not working

---
+++ pocket-calculator.js
@@ -0,0 +1,8 @@
+//val is the value of the numbers DO NOT CHNAGE TO EACH NUMBER
+function dis(val)  {
+       document.getElementById("result").value+=val
+}
+
+function clr() {
+    document.getElementById("result").value =" "
+  }
