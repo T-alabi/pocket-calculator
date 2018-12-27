30d503622181503e45dc565219ba64a98698a859
First Last <youremailaddress@example.com>
2018-12-27 14:13:35 -0500

    clear, and solve function added and working

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -6,3 +6,9 @@ function dis(val)  {
 function clr() {
     document.getElementById("result").value =" "
   }
+
+function solve() {
+  let x = document.getElementById("result").value
+  let y = eval(x);
+  document.getElementById("result").value = y
+}
