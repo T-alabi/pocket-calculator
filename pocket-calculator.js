ec00211e5360d527bdede1bbc8e2cf17341a7865
Tunmise Alabi <talabi@ucvts.tec.nj.us>
2019-01-07 13:22:44 -0500

    percent function added but not tested

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -7,15 +7,32 @@ function dis(val)  {
 function dot(val){
 //placeholder keeps the zero after the decimal point
 //do later (decimal point)
-let disval = "yes";
 document.getElementById("result").value+=val+=val.placeholder="0";
 }
 
 function clr() {
     document.getElementById("result").value =" "
   }
+function per(val) {
+  let x = document.getElementById("result").value
+  let y = eval(x)/100;
+ document.getElementById("result").value = y
+ if (isNaN(y) ) {
+  document.getElementById("result").value = "Error";
+}
+/*thousands place commas*/
+if (y > 999){
+  document.getElementById("result").value = y.toLocaleString();
+}
+/*scientific notation */
+if (y > 999999999){
+    document.getElementById("result").value = y.toExponential();
+}
+}
 
 function solve() {
+  /*divid the number with the percent by 100 beore anything else. use division */
+  //x% should be x/100
   let x = document.getElementById("result").value
   let y = eval(x);
  document.getElementById("result").value = y
@@ -23,7 +40,6 @@ function solve() {
  if (isNaN(y) ) {
   document.getElementById("result").value = "Error";
 }
-
 /*thousands place commas*/
 if (y > 999){
   document.getElementById("result").value = y.toLocaleString();
