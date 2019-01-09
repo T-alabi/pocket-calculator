7255bcb3a48e50b88375317afcd12b543c8cc0d4
Tunmise Alabi <talabi@ucvts.tec.nj.us>
2019-01-09 13:08:39 -0500

    decimal zero still showing

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,18 +1,21 @@
+function dot(val){
+//placeholder keeps the zero after the decimal point
+//do later (decimal point)
+document.getElementById("result").innherHTML+=val+="0";
+if (dis(val)){
+  document.getElementById("result").innerHTML-=val+val
+}
+}
 
-//val is the value of the numbers DO NOT CHNAGE TO EACH NUMBER
 function dis(val)  {
        document.getElementById("result").value+=val
 }
 
-function dot(val){
-//placeholder keeps the zero after the decimal point
-//do later (decimal point)
-document.getElementById("result").value+=val+=val.placeholder="0";
-}
 
 function clr() {
     document.getElementById("result").value =" "
   }
+
 function per(val) {
   let x = document.getElementById("result").value
   let y = eval(x)/100;
@@ -31,8 +34,6 @@ if (y > 999999999){
 }
 
 function solve() {
-  /*divid the number with the percent by 100 beore anything else. use division */
-  //x% should be x/100
   let x = document.getElementById("result").value
   let y = eval(x);
  document.getElementById("result").value = y
