017e49d7ccf03ba0023231b70bc40fec7270e0e4
First Last <youremailaddress@example.com>
2019-01-12 16:39:12 -0500

    failed test cases identified and 1 fixed

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -51,4 +51,17 @@ if (y > 999){
 if (y > 999999999){
     document.getElementById("result").value = y.toExponential();
 }
+if(isFinite(y)===false ){
+  document.getElementById("result").value = "Error"
 }
+}
+/*FAILED TEST CASES
+Enter 5 + - 3, and your calculator should display 2.
+Enter 8 + x 2, and your calculator should display 16.
+Enter 8 + / 4, and your calculator should display 2.
+1.2.3.4, and your calculator should display 1.234.
+(doesnt evaluate for all 4)
+Enter 7 and then press AC, and your calculator should display 0(input screen blank)
+Enter 9 and then +/-, and your calculator should display -9(shows9)
+
+*/
