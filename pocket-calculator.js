e8ba1c2f7791273730eddc77120ec1ea2557202f
Tunmise Alabi <talabi@ucvts.tec.nj.us>
2019-01-22 13:08:38 -0500

    decimals and display

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,3 +1,5 @@
+let decimalCount = 0;
+
 function removeLeadingZero(val) {
 /*  let notRan;
   notRan = true;
@@ -13,23 +15,16 @@ function dis(val)  {
 //removeLeadingZero(val);
    document.getElementById("result").value+=val
    console.log(val);
-   /*idek*/
-  let num = document.getElementById("result").value
-  if (num >= 999){
-    document.getElementById("result").value.toLocaleString();
-  }
-
 }
 
 function dot(val) {
 //placeholder keeps the zero after the decimal point DEAL WIT IT LATER
-document.getElementById("result").value+=val
 
-let decimalCount = 0;
-decimalCount = decimalCount+ 1;
-console.log("there are", decimalCount)
-/*works but you can oly use the decimal in one number*/
-if (decimalCount >= 1){
+document.getElementById("result").value+=val
+decimalCount = decimalCount+1;
+console.log("Count is:", decimalCount)
+/*works but you can oly use 2 decimals*/
+if (decimalCount >= 2){
   var button = document.getElementById("decimal")
 button.disabled = true;
 }
@@ -51,29 +46,23 @@ function solve() {
   let x = document.getElementById("result").value
   let y = eval(x);
  document.getElementById("result").value = y
-/*error message*/
+/*error message NAN*/
  if (isNaN(y) ) {
   document.getElementById("result").value = "Error";
 }
-/*thousands place commas*/
-if (y > 999){
+/*scientific notation */
+if (y > 999999999 ){
+    document.getElementById("result").value = y.toExponential()
+}else if   (y > 999){
   document.getElementById("result").value = y.toLocaleString();
 }
-/*scientific notation */
-if (y > 999999999){
-    document.getElementById("result").value = y.toExponential();
-}
-/*infinity error message*/
+/*infinity error message not working*/
 if(isFinite(y)===false){
   document.getElementById("result").value+= "Error"
 }
 /*NAN error message*/
 if (isNaN(y) ) {
  document.getElementById("result").value = "Error";
-}
-/*thousands place commas*/
-if (y > 999){
- document.getElementById("result").value = y.toLocaleString();
 }
 
 }
@@ -88,7 +77,7 @@ function makeCurrentValueNegative(val) {
 /*FAILED TEST CASES
 Enter 8 + x 2, and your calculator should display 16.
 Enter 8 + / 4, and your calculator should display 2.
-1.2.3.4, and your calculator should display 1.234.
+1.2.3.4, and your calculator should display 1.234.(Kinda solved)
 (doesnt evaluate for all 4)
 Enter 7 and then press AC, and your calculator should display 0 (works now but doesnt go away)
 */
