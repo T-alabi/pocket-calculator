dfb35c291b6a68476bd753669ed882b8590ca956
Tunmise Alabi <talabi@ucvts.tec.nj.us>
2019-01-22 13:23:21 -0500

    decimals and display

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -7,11 +7,15 @@ if( notRan===true){
   document.getElementById("result").value+=""
 }*/
 //value-=0 gives the NAN(val) so don't
-
+}
+/*negative symbol (+/-) and negative*/
+function makeCurrentValueNegative(val) {
+  let x = document.getElementById("result").value
+  let negVal = x*-1
+  document.getElementById("result").value = negVal
 }
 
 function dis(val)  {
-
 //removeLeadingZero(val);
    document.getElementById("result").value+=val
    console.log(val);
@@ -57,8 +61,8 @@ if (y > 999999999 ){
   document.getElementById("result").value = y.toLocaleString();
 }
 /*infinity error message not working*/
-if(isFinite(y)===false){
-  document.getElementById("result").value+= "Error"
+if (isFinite(y)==false){
+  document.getElementById("result").value= "Error"
 }
 /*NAN error message*/
 if (isNaN(y) ) {
@@ -66,17 +70,13 @@ if (isNaN(y) ) {
 }
 
 }
-/*negative symbol (+/-) and negative*/
-function makeCurrentValueNegative(val) {
-  let x = document.getElementById("result").value
-  let negVal = x*-1
-  document.getElementById("result").value = negVal
-}
 
 
 /*FAILED TEST CASES
 Enter 8 + x 2, and your calculator should display 16.
 Enter 8 + / 4, and your calculator should display 2.
+have opperators seperately
+
 1.2.3.4, and your calculator should display 1.234.(Kinda solved)
 (doesnt evaluate for all 4)
 Enter 7 and then press AC, and your calculator should display 0 (works now but doesnt go away)
